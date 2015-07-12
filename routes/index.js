8de5578b990d3164b156df4a8ab6f786184ab231
VanesaGC <vanesa.garcia.c@gmail.com>
2015-07-12 10:26:31 +0100

    Ejercicio parte 2

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz: Créditos',
+                        author: 'Vanesa García Campos',
+                        /*photo: 'images/foto.png'*/});
+});
+
 module.exports = router;
 
 router.get('/quizes/question', quizController.question);
